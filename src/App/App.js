ed64592146b5e855bdd7b1d617d5a6a8ecd19447
LordAldi <aldianugra09@gmail.com>
2021-05-11 12:06:22 +0700

    basic setting

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SideMenu from "../components/SideMenu";
+function App() {
+  return (
+    <>
+      <SideMenu />
+      <div>yeess</div>
+    </>
+  );
+}
+
+export default App;

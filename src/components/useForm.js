419e5f8695dc7ab498d60c80cf07cb5024f34761
LordAldi <aldianugra09@gmail.com>
2021-05-14 00:38:25 +0700

    single control validation, employee service(inser, getall,generateid)

--- src/components/useForm.js
+++ src/components/useForm.js
@@ -1,12 +1,14 @@
 import { makeStyles } from "@material-ui/core";
 import React, { useState } from "react";
 
-const useForm = (initialState) => {
+const useForm = (initialState, validateOnChange = false, validate) => {
   const [values, setValues] = useState(initialState);
   const [errors, setErrors] = useState({});
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+
+    if (validateOnChange) validate({ [name]: value });
   };
   const resetForm = () => {
     setValues(initialState);
--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -22,23 +22,33 @@ const genderItems = [
 ];
 
 const EmployeeForm = () => {
-  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
-    useForm(initialFValues);
+  const validate = (fieldValues = values) => {
+    let temp = { ...errors };
+    if ("fullName" in fieldValues)
+      temp.fullName = fieldValues.fullName ? "" : "This field is required.";
+    if ("email" in fieldValues)
+      temp.email = /$^|.+@.+..+/.test(fieldValues.email)
+        ? ""
+        : "Email is not valid.";
+    if ("mobile" in fieldValues)
+      temp.mobile =
+        fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required.";
+    if ("departmentId" in fieldValues)
+      temp.departmentId =
+        fieldValues.departmentId.length != 0 ? "" : "This field is required.";
+    setErrors({
+      ...temp,
+    });
 
-  const validate = () => {
-    let temp = {};
-    temp.fullName = values.fullName ? "" : "This Field is required";
-    temp.email = /$^|.+@+..+/.test(values.email) ? "" : "Email is not valid";
-    temp.mobile = values.mobile.length > 9 ? "" : "Minimun 10 number required";
-    temp.departmentId =
-      values.departmentId.length != 0 ? "" : "This field is required";
-    setErrors({ ...temp });
-    return Object.values(temp).every((x) => x == "");
+    if (fieldValues == values) return Object.values(temp).every((x) => x == "");
   };
+  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
+    useForm(initialFValues, true, validate);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validate()) {
-      window.alert("testtt");
+      employeeService.insertEmployee(values);
+      resetForm();
     }
   };
   return (
